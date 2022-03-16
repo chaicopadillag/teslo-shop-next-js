026728ff55ea781af141711320a66451c51243c5
Chaico Padilla G <chaicopadillag@gmail.com>
2022-03-16 09:05:13 -0500

    implement auth pages

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -3,15 +3,18 @@ import NextLink from 'next/link';
 import React from 'react';
 import { AuthLayout } from '../../components/layouts';
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
     <AuthLayout title='Login'>
       <Box sx={{ width: 350, padding: '10px 20px' }}>
         <Grid container spacing={2}>
           <Grid item xs={12}>
             <Typography variant='h1' component='h1' textAlign='center'>
-              Iniciar Sesión
+              Registrarse
             </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField label='Nombre completo' type='text' variant='filled' fullWidth />
           </Grid>
           <Grid item xs={12}>
             <TextField label='Correo' type='email' variant='filled' fullWidth />
@@ -21,12 +24,12 @@ const LoginPage = () => {
           </Grid>
           <Grid item xs={12}>
             <Button color='secondary' className='circular-btn' size='large' fullWidth>
-              Iniciar
+              Crear cuenta
             </Button>
           </Grid>
           <Grid item xs={12} display='flex' justifyContent='center'>
-            <NextLink href='/auth/register' passHref>
-              <Link underline='hover'>¿No tienes una cuenta? Regístrate</Link>
+            <NextLink href='/auth/login' passHref>
+              <Link underline='hover'>¿Ya tienes una cuenta? Inicia sesión</Link>
             </NextLink>
           </Grid>
         </Grid>
@@ -35,4 +38,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
